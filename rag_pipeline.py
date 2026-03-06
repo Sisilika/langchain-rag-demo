53a0d62a9dee98326be4abaac08e32ee5dc890cb
Sisilika <97743633+Sisilika@users.noreply.github.com>
2026-03-07 02:26:59 +0530

    rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,33 +1,34 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
 import tempfile
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+from langchain_community.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
 
 
 def process_document(uploaded_file):
 
-    # Create embedding model INSIDE function
-    embedding_model = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
-
     # Save uploaded file temporarily
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.write(uploaded_file.read())
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        temp_file.write(uploaded_file.read())
+        temp_file_path = temp_file.name
 
-    loader = PyPDFLoader(temp_file.name)
+    loader = PyPDFLoader(temp_file_path)
     docs = loader.load()
 
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50
+        chunk_size=800,
+        chunk_overlap=100
     )
 
     chunks = splitter.split_documents(docs)
 
-    vectorstore = FAISS.from_documents(chunks, embedding_model)
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
-    retriever = vectorstore.as_retriever(search_kwargs={"k":3})
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+
+    retriever = vectorstore.as_retriever()
 
     return retriever, chunks
